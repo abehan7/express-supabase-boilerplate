d4695e09b2fea44e1726513f9a8b0df12024e8f6
abe0707 <hanjk13262@gmail.com>
2024-08-06 20:14:23 +0900

    Update: remove user

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import db from './models/index.js';
-// import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
-
-import { createClient } from '@supabase/supabase-js';
 
 dotenv.config();
 
@@ -16,22 +13,7 @@ app.use(bodyParser.json());
 db.sequelize.sync();
 
 // Routes
-// app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
-
-// Supabase setup
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-app.get('/supabase', async (req, res) => {
-  const { data, error } = await supabase.from('Users').select('*');
-  if (error) {
-    res.status(500).json({ error: error.message });
-  } else {
-    res.json(data);
-  }
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 import Sequelize from 'sequelize';
 import sequelize from '../config/database.js';
-import userModel from './user.js';
 import postModel from './post.js';
 
 const db = {};
@@ -9,7 +8,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // Import models
-db.users = userModel(sequelize, Sequelize);
 db.posts = postModel(sequelize, Sequelize);
 
 export default db;
